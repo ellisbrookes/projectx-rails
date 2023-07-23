1a183aec6b24e6ae2f6a2205727282cc1416bdcf
Ellis Brookes <ellis.brookes@bagley.studio>
2023-07-23 23:19:02 +0100

    Created team views, controller and migration

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,8 +1,54 @@
 class TeamsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_team, only: %i[ show edit update destroy]
+
   layout 'dashboard'
 
   def index
-     @teams = Team.all
+    @teams = Team.all
   end
+
+  def show
+  end
+
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.new(team_params)
+
+    if @team.save
+      redirect_to team_url(@team), notice: "Team was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @team.update(team_params)
+      redirect_to team_url(@team), notice: "Team was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @team.destroy
+    
+    redirect_to teams_url, notice: "Team was successfully destroyed"
+  end
+
+  private
+
+    def set_team
+      @team = Team.find(params[:id])
+    end
+
+    def team_params
+      params.require(:team).permit(:name, :description, :team_email)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_17_231516) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_23_200231) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_17_231516) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_companies_on_user_id"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "team_email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
