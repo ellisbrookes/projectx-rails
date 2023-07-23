7e2c58504ccc0bbbf3950cced1eba5360cbee72a
Ellis Brookes <ellis.brookes@bagley.studio>
2023-07-23 23:39:41 +0100

    Removed companies schema

--- db/schema.rb
+++ db/schema.rb
@@ -41,17 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_23_200231) do
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
-
-  create_table "companies", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.string "email"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_companies_on_user_id"
-  end
-
+  
   create_table "teams", force: :cascade do |t|
     t.string "name"
     t.text "description"
