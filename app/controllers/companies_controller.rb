1cecd7ff3ca0a349896fa3021be1f208636630bf
Ben Bagley <ben.bagley@bbc.co.uk>
2023-07-23 12:34:00 +0100

    Fixed duplicate company creation on edit

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -29,8 +29,6 @@ class CompaniesController < ApplicationController
   end
 
   def update
-    @company = current_user.companies.build(company_params)
-
     if @company.update(company_params)
       redirect_to company_url(@company), notice: "Company was successfully updated."
     else
