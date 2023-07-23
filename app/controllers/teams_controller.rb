fca4c26bbb88fd1088bd153682b88ef431d809d3
Ellis Brookes <ellis.brookes@bagley.studio>
2023-07-23 20:36:47 +0100

    Routes now working and teams page is rendering

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,5 +1,5 @@
-class TeamsController < ApplicationControlle
-  before_action authenticate_user!
+class TeamsController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :show]
   layout 'dashboard'
 
   def index
