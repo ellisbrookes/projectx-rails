2eef7e36e9a7f27b9488c28eea6e620d919a999f
Ellis Brookes <ellis.brookes@bagley.studio>
2023-08-01 02:21:59 +0100

    Added validations

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,8 @@
 # app/models/team.rb
 class Team < ApplicationRecord
   belongs_to :company
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :team_email, presence: true
 end
