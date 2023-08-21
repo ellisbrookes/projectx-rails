2d69a4a64ef10779f21ccfb5832166eae08e9634
Ellis Brookes <ellis.brookes@bagley.studio>
2023-08-21 16:38:42 +0100

    lint fixes

--- app/controllers/darkmode_controller.rb
+++ app/controllers/darkmode_controller.rb
@@ -1,7 +1,7 @@
 class DarkmodeController < ApplicationController
   def update_dark_mode_preference
     session[:dark_mode] = params[:dark_mode]
-    
-    render json: { dark_mode: session[:dark_mode] }
+
+    render(json: { dark_mode: session[:dark_mode] })
   end
 end
