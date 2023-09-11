b1e86b78df53c25e0c2cd1e0c2a75b809eb835fb
Ellis Brookes <ellis.brookes@bagley.studio>
2023-09-11 02:00:40 +0100

    dark mode toggle changes & js changes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -31,4 +31,63 @@ darkModeToggle.addEventListener('click', function () {
       localStorage.theme = 'dark'
     }
   }
+
+  const button = document.querySelector('label')
+  const iconsWrapper = button?.querySelector('.icons-wrapper')
+  const sun = iconsWrapper?.querySelector('.sun-span')
+  const moon = iconsWrapper?.querySelector('.moon-span')
+
+  button?.addEventListener('click', () => {
+    // replace background
+    if (button.classList.contains('bg-gray-200')) {
+      button?.classList.replace('bg-gray-200', 'bg-indigo-400')
+    } else {
+      button?.classList.replace('bg-indigo-400', 'bg-gray-200')
+    }
+
+    // move toggle slider
+    if (iconsWrapper?.classList.contains('translate-x-0')) {
+      iconsWrapper?.classList.replace('translate-x-0', 'translate-x-5')
+    } else {
+      iconsWrapper?.classList.replace('translate-x-5', 'translate-x-0')
+    }
+
+    // toggle sun-span icon, opacity and duration
+    if (sun?.classList.contains('opacity-100')) {
+      sun?.classList.replace('opacity-100', 'opacity-0')
+    } else {
+      sun?.classList.replace('opacity-0', 'opacity-100')
+    }
+
+    if (sun?.classList.contains('duration-200')) {
+      sun?.classList.replace('duration-200', 'duration-100')
+    } else {
+      sun?.classList.replace('duration-100', 'duration-200')
+    }
+
+    if (sun?.classList.contains('ease-in')) {
+      sun?.classList.replace('ease-in', 'ease-out')
+    } else {
+      sun?.classList.replace('ease-out', 'ease-in')
+    }
+
+    // toggle moon-span, opacity and duration
+    if (moon?.classList.contains('opacity-0')) {
+      moon?.classList.replace('opacity-0', 'opacity-100')
+    } else {
+      moon?.classList.replace('opacity-100', 'opacity-0')
+    }
+
+    if (moon?.classList.contains('duration-100')) {
+      moon?.classList.replace('duration-100', 'duration-200')
+    } else {
+      moon?.classList.replace('duration-200', 'duration-100')
+    }
+
+    if (moon?.classList.contains('ease-out')) {
+      moon?.classList.replace('ease-out', 'ease-in')
+    } else {
+      moon?.classList.replace('ease-in', 'ease-out')
+    }
+  })
 })
