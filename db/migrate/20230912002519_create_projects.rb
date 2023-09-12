46f057e4e3ea7571c37966401e0e235d370b5693
Ellis Brookes <ellis.brookes@bagley.studio>
2023-09-12 19:47:47 +0100

    added esimated completion date and completion date to migration

--- db/migrate/20230912002519_create_projects.rb
+++ db/migrate/20230912002519_create_projects.rb
@@ -1,8 +1,10 @@
 class CreateProjects < ActiveRecord::Migration[7.0]
   def change
-    create_table :projects do |t|
+    create_table(:projects) do |t|
       t.string :name
       t.string :description
+      t.date :estimated_completion_date
+      t.date :completion_date
 
       t.timestamps
     end
