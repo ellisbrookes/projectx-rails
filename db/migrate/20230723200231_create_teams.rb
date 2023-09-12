8c5dd954a017cfd0b15e3f7ed0d79386602a1c63
Ellis Brookes <ellis.brookes@bagley.studio>
2023-09-12 02:15:26 +0100

    never mind doesn't fix it

--- db/migrate/20230723200231_create_teams.rb
+++ db/migrate/20230723200231_create_teams.rb
@@ -1,7 +1,6 @@
 class CreateTeams < ActiveRecord::Migration[7.0]
   def change
     create_table :teams do |t|
-      t.references :company, foreign_key: { on_delete: :cascade }
       t.string :name
       t.text :description
       t.string :team_email
