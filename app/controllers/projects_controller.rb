7cf5964b2bc4e331471d5e4b69ec06c965a7a9b7
Ellis Brookes <ellisbrookes98@gmail.com>
2023-09-22 19:07:11 +0100

    added before_action :authenticate_user!

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,8 @@
 class ProjectsController < ApplicationController
   layout 'dashboard'
+  before_action :authenticate_user!
   before_action :set_project, only: [:show, :edit, :update, :destroy]
+
 
   def index
     @projects = Project.all
