bfd1ca1a2e21baa5dcf422b344445f426f978fa2
Ellis Brookes <ellisbrookes98@gmail.com>
2023-09-22 19:41:44 +0100

    lint fixes

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,7 +3,6 @@ class ProjectsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
-
   def index
     @projects = Project.all
   end
