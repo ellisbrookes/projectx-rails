50da405e4c558d1b32c154cf62194345229a076b
Ellis Brookes <ellisbrookes98@gmail.com>
2023-10-13 20:54:35 +0100

    updated model

--- app/models/subtask.rb
+++ app/models/subtask.rb
@@ -1,3 +1,6 @@
 class Subtask < ApplicationRecord
-  belongs_to :task
+  belongs_to :reporter, class_name: "User"
+  belongs_to :assigned_to, class_name: "User"
+  belongs_to :project
+  belongs_to :team
 end
