df79654b5cf9ef8b6c9769bedfe2c28ebb3c4202
Ellis Brookes <ellisbrookes98@gmail.com>
2023-10-19 01:53:15 +0100

    lint fixes

--- app/components/heading_component.rb
+++ app/components/heading_component.rb
@@ -2,10 +2,11 @@
 
 class HeadingComponent < ViewComponent::Base
   def initialize(title:, text:, link_title:, link_path:)
+    super
+
     @title = title
     @text = text
     @link_title = link_title
     @link_path = link_path
   end
-
 end
