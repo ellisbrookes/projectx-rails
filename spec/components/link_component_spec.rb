5cba089e1f111c03708bbba436250a3c6f2ecebf
Ellis Brookes <ellisbrookes98@gmail.com>
2023-10-19 02:25:23 +0100

    more lint fixes

--- spec/components/link_component_spec.rb
+++ spec/components/link_component_spec.rb
@@ -2,7 +2,7 @@
 
 require "rails_helper"
 
-RSpec.describe LinkComponent, type: :component do
+RSpec.describe(LinkComponent, type: :component) do
   pending "add some examples to (or delete) #{__FILE__}"
 
   # it "renders something useful" do
