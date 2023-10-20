6f6991f621af5ad44a1a4921dd5eb47277657b34
Ellis Brookes <ellisbrookes98@gmail.com>
2023-10-21 00:53:49 +0100

    prettier changes

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,34 +1,34 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}"
   ],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans]
       },
       colors: {
         nepal: {
-          50: '#f6f8f9',
-          100: '#ebf1f3',
-          200: '#d3e0e4',
-          300: '#acc7cd',
-          400: '#91b5bc',
-          500: '#5f8e98',
-          600: '#4b747e',
-          700: '#3d5e67',
-          800: '#365056',
-          900: '#30444a'
+          50: "#f6f8f9",
+          100: "#ebf1f3",
+          200: "#d3e0e4",
+          300: "#acc7cd",
+          400: "#91b5bc",
+          500: "#5f8e98",
+          600: "#4b747e",
+          700: "#3d5e67",
+          800: "#365056",
+          900: "#30444a"
         }
       },
       boxShadow: {
-        hardened: '3px 3px 0 rgb(0,0,0)'
+        hardened: "3px 3px 0 rgb(0,0,0)"
       },
       keyframes: {
         fadeOut: {
@@ -37,14 +37,14 @@ module.exports = {
         }
       },
       animation: {
-        fadeOut: 'fadeOut 1s ease-in-out'
+        fadeOut: "fadeOut 1s ease-in-out"
       }
     }
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries')
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/container-queries")
   ]
 }
