bc4387cd9b98bad1e3c909797bbf78548b3a2204
Ellis Brookes <ellisbrookes98@gmail.com>
2023-11-22 03:26:45 +0000

    more database seeds not working fully yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,21 @@
 
 # Clear existing data from the users table
 User.destroy_all
+Project.destroy_all
 
 # Create two users with full names
-User.create(full_name: 'John Doe', email: 'john@example.com', password: 'password123')
-User.create(full_name: 'Jane Doe', email: 'jane@example.com', password: 'password456')
+user1 = User.create(full_name: 'John Doe', email: 'john@example.com', password: 'password123')
+user2 = User.create(full_name: 'Jane Doe', email: 'jane@example.com', password: 'password456')
 
 puts 'Seeded database with users: John Doe and Jane Doe.'
+
+# Create tasks with description etc.
+Task.create(name: 'Sort out DNS', description: 'Login to Namecheap', due_date: '23/11/2023', project: 'ProjectX', status: 'pending', reporter: 'John Doe', assigned_to: 'John Doe', team: 'Development')
+
+puts 'Seeded database with Task: Sort out DNS'
+
+# Create project
+Project.create(name: 'ProjectX', user: user1)
+Project.create(name: 'ProjectY', user: user2)
+
+puts 'Seeded database with Projects: ProjectX and ProjectY'
