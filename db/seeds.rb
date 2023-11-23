fcbf578057b5bc1ddce498288158c785809a9a44
Ellis Brookes <ellisbrookes98@gmail.com>
2023-11-23 22:33:09 +0000

    added seed for team and project

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,22 +13,46 @@ u = User.create(full_name: Faker::Name.name, email: Faker::Internet.email, passw
 u.skip_confirmation!
 u.save
 
-puts 'Seeded 5 into the database'
+puts 'Seeded 4 users and 1 admin user into the database'
 
 # Create a company with a title and description
 10.times do |n|
   Company.create(name: Faker::Company.name, description: Faker::Company.catch_phrase, email: Faker::Internet.email, user_id: Faker::Number.within(range: 1..6))
 end
 
-puts 'Seeded 10 into the database'
+puts 'Seeded 10 companies into the database'
 
-# Create project
-Project.create(title: 'ProjectX')
-Project.create(title: 'ProjectY')
 
-# puts 'Seeded database with Projects: ProjectX and ProjectY'
-
-# # Create tasks with description etc.
-# Task.create(name: 'Sort out DNS', description: 'Login to Namecheap', due_date: '23/11/2023', project: 'ProjectX', status: 'pending', reporter: 'John Doe', assigned_to: 'John Doe', team: 'Development')
-
-# puts 'Seeded database with Task: Sort out DNS'
+# Create team
+10.times do |n|
+  Team.create(
+    name: Faker::Team.name,
+    description: Faker::Company.catch_phrase,
+    team_email: Faker::Internet.email,
+    company_id: Faker::Number.within(range: 1..6)
+    )
+  end
+  
+  puts 'Seeded 10 teams in the database'
+  
+  # Create project
+  10.times do |n|
+    Project.create(
+      title: Faker::Company.name,
+      description: Faker::Company.catch_phrase,
+      start_date: Faker::Date.backward(days: 30),
+      completion_date: Faker::Date.forward(days: 30),
+      team_id: Faker::Number.within(range: 1..6),
+      company_id: Faker::Number.within(range: 1..6),
+      estimated_budget: Faker::Commerce.price,
+      actual_budget: Faker::Commerce.price
+    )
+  end
+  
+  puts 'Seeded 10 projects into the database'
+  
+  # Create tasks with description etc.
+  # Task.create(name: 'Sort out DNS', description: 'Login to Namecheap', due_date: '23/11/2023', project: 'ProjectX', status: 'pending', reporter: 'John Doe', assigned_to: 'John Doe', team: 'Development')
+  
+  # puts 'Seeded database with Task: Sort out DNS'
+  
