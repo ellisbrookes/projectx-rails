2143ae9975dba5f41690d3ab30297b575baf0e5d
Ellis Brookes <ellisbrookes98@gmail.com>
2023-12-07 01:45:32 +0000

    team changes and updates to team_members controller

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,7 @@
 class Team < ApplicationRecord
   belongs_to :company
   has_many :team_members, dependent: :destroy
+  has_many :users, through: :team_members
   has_many :projects
   accepts_nested_attributes_for :team_members
 
