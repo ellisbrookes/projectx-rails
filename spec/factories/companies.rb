9ef417fb67d02444da59e75e8774bb22d01d7354
Ellis Brookes <ellisbrookes98@gmail.com>
2023-12-21 01:51:25 +0000

    lint fixes

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -1,9 +1,8 @@
 FactoryBot.define do
-
   factory :company do
     name { Faker::Company.name }
     description { Faker::Company.catch_phrase }
-    email { Faker::Internet.email } 
+    email { Faker::Internet.email }
     user_id { 1 }
   end
 end
--- spec/requests/companies_spec.rb
+++ spec/requests/companies_spec.rb
@@ -19,45 +19,45 @@ RSpec.describe("Companies", type: :request) do
   describe "POST /new" do
     it "Should be able to render new company page" do
       get new_company_path
-      expect(response).to render_template(:new)
+      expect(response).to(render_template(:new))
     end
 
     it "Should be able to create a company" do
       get new_company_path
-      expect(response).to render_template(:new)
-      
+      expect(response).to(render_template(:new))
+
       # Create the company
       company_params = FactoryBot.attributes_for(:company)
       post companies_path, params: { company: company_params }
 
       # Redirect to company
-      expect(response).to have_http_status(:redirect)
+      expect(response).to(have_http_status(:redirect))
       follow_redirect!
 
       # Render the show page
-      expect(response).to render_template(:show)
-      expect(response.body).to include("Company was successfully created.")
-      
+      expect(response).to(render_template(:show))
+      expect(response.body).to(include("Company was successfully created."))
+
       # Testing company data
-      expect(response.body).to include(company_params[:name])
-      expect(response.body).to include(company_params[:description])
-      expect(response.body).to include(company_params[:email])
+      expect(response.body).to(include(company_params[:name]))
+      expect(response.body).to(include(company_params[:description]))
+      expect(response.body).to(include(company_params[:email]))
     end
 
     it "should not be able to create a company" do
       get new_company_path
-      expect(response).to render_template(:new)
+      expect(response).to(render_template(:new))
 
       company_params = FactoryBot.attributes_for(:company, email: "")
-      post companies_path, params: {company: company_params}
+      post companies_path, params: { company: company_params }
 
-      expect(response).to have_http_status(:unprocessable_entity)
+      expect(response).to(have_http_status(:unprocessable_entity))
 
-      expect(response).to render_template(:new)
+      expect(response).to(render_template(:new))
 
       error_message = "Email can't be blank"
-      decoded_error_message = CGI::unescapeHTML(error_message)
-      expect(response.body).to include(decoded_error_message)
+      decoded_error_message = CGI.unescapeHTML(error_message)
+      expect(response.body).to(include(decoded_error_message))
     end
   end
 end
