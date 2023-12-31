8069b0d5194f0311f89524320a93b1b0cc2f903f
Ellis Brookes <ellisbrookes98@gmail.com>
2023-12-31 15:04:38 +0000

    registrations_controller changes

--- app/controllers/registrations/registrations_controller.rb
+++ app/controllers/registrations/registrations_controller.rb
@@ -49,7 +49,7 @@ class Registrations::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar, :full_name])
   end
 
   # The path used after sign up.
