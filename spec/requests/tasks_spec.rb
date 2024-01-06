c1f4f78877c181da09eb5ad410d61ac786687c03
Ellis Brookes <ellisbrookes98@gmail.com>
2024-01-06 04:08:32 +0000

    task_spec updates

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -66,4 +66,77 @@ RSpec.describe("Tasks", type: :request) do
       expect(response.body).to(include("Task name can&#39;t be blank"))
     end
   end
+
+  describe "PUT /edit" do
+    before do
+      get new_company_project_task_path(company_id: @company.id, project_id: @project.id)
+      expect(response).to(render_template(:new))
+
+      # create the task
+      task_params = FactoryBot.attributes_for(:task, team_members_attributes: [{ user_id: @user.id }], company_id: @company.id, project_id: @project.id)
+      post company_project_task_path(company_id: @company.id, project_id: @project.id)
+
+      # redirect to the team
+      expect(response).to(have_http_status(:redirect))
+      follow_redirect!
+    end
+
+    it "Should be able to update a task" do
+      @task = Task.first
+      get edit_company_project_task_path(company_id: @company.id, project_id: @project.id)
+      expect(response).to(render_template(:edit))
+
+      # update name
+      new_name = Faker::Company.name
+      task_params = { task: { task_name: new_name }}
+      put company_project_task_path(company_id: @company.id, project_id: @project.id), params: task_params
+
+      # redirect to the task
+      expect(response).to(have_http_status(:redirect))
+      follow_redirect!
+
+      # render show page
+      expect(response).to(render_template(:show))
+      expect(response.body).to(include(new_name))
+      expect(response.body).to(include("Task was successfully updated."))
+    end
+
+    it "Should not be able to update a task" do
+      @task = Task.first
+      get edit_company_project_task_path(company_id: @company.id, project_id: @project.id)
+      expect(response).to(render_template(:edit))
+
+      # update task without a name
+      task_params = FactoryBot.attributes_for(:task, name: "", team_members_attributes: [{ user_id: @user.id }], company_id: @company.id, project_id: @project.id)
+      put company_project_task_path(company_id: @company.id, project_id: @project.id), params: task_params
+
+      # render error message
+      expect(response).to(render_template(:unprocessable_entity))
+
+      # render task edit page
+      expect(response).to(render_template(:edit))
+
+      # render error message saying name can't be blank
+      expect(response.body).to(include("Task name can&#39;t be blank"))
+    end
+  end
+
+  describe "DELETE /show" do
+    it "Should be able to delete a task"
+    @task = FactoryBot.create(:task, project_id: @project.id, team_id: @team.id)
+
+    get company_project_task_path(company_id: @company.id, project_id: @project.id)
+    expect(response).to(render_template(:show))
+
+    # delete task
+    delete company_project_task_path(company_id: @company.id, project_id: @project.id)
+
+    # redirect to task
+    expect(response).to(have_http_status(:redirect))
+    follow_redirect!
+
+    # redirect to the index page
+    expect(response).to(render_template(:index))
+    expect(response.body).to(include("Task was successfully destroyed."))
+  end
 end
