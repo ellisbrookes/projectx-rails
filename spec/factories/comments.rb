fa9fd987e88735e8c9f20372405060c6d96be850
Ellis Brookes <ellisbrookes98@gmail.com>
2024-01-10 21:27:59 +0000

    changes

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :comment do
-    body { Faker::Lorem.sentence(word_count: 20) }
+    body { Faker::Lorem.sentence(word_count: 10).gsub(/[^0-9a-zA-Z\s]/, '') }
     user_id { 1 }
     task_id { 1 }
   end
