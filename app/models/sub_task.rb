402ab89399d85754905613ae1e44596979957b1b
Ellis Marcus Brookes <ellisbrookes98@gmail.com>
2024-01-20 23:25:17 +0000

    Sub-tasks tests (#26)

--- app/models/sub_task.rb
+++ app/models/sub_task.rb
@@ -1,7 +1,19 @@
 class SubTask < ApplicationRecord
+  extend FriendlyId
+  friendly_id :name, use: :slugged
+
+  def should_generate_new_friendly_id?
+    slug.blank? || name_changed?
+  end
+
   belongs_to :task
   belongs_to :reporter, class_name: "User"
   belongs_to :assigned_to, class_name: "User"
   belongs_to :project
   belongs_to :team
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :due_date, presence: true
+  validates :status, presence: true
 end
