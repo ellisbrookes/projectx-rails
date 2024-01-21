7622bb8c9f376bb4b035e99ecb5d96b88d6325bd
Ben Bagley <hello@benbagley.dev>
2024-01-21 02:10:12 +0000

    fixed theme toggle

--- app/javascript/controllers/theme_controller.js
+++ app/javascript/controllers/theme_controller.js
@@ -7,20 +7,35 @@ export default class extends Controller {
   connect() {
     if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
       this.lightIconTarget.classList.remove('hidden');
+
+      // animation classes
       this.switcherTarget.classList.remove('translate-x-0');
       this.switcherTarget.classList.add('translate-x-5');
+
+      // button bg colour classes
+      this.themeToggleTarget.classList.remove('bg-gray-200');
+      this.themeToggleTarget.classList.add('bg-nepal-500');
+
+      // force dark mode when switched to dark but system preferences are dark mode
+      document.documentElement.classList.add('dark');
     } else {
       this.darkIconTarget.classList.remove('hidden');
+
+      // animation classes
       this.switcherTarget.classList.remove('translate-x-5');
       this.switcherTarget.classList.add('translate-x-0');
+
+      // button bg colour classes
+      this.themeToggleTarget.classList.remove('bg-nepal-500');
+      this.themeToggleTarget.classList.add('bg-gray-200');
+
+      // force light mode when switched to light but system preferences are light mode
+      document.documentElement.classList.remove('dark');
     }
   }
 
   toggleTheme() {
     console.log('theme target clicked')
-
-    // < !--lightEnabled: "opacity-0 duration-100 ease-out", Not Enabled: "opacity-100 duration-200 ease-in" -- >
-    // < !--darkEnabled: "opacity-100 duration-200 ease-in", Not Enabled: "opacity-0 duration-100 ease-out" -- >
 
     this.lightIconTarget.classList.toggle('hidden');
     this.darkIconTarget.classList.toggle('hidden');
@@ -31,14 +46,26 @@ export default class extends Controller {
         localStorage.setItem('theme', 'dark');
 
         document.documentElement.classList.add('dark');
+
+        // animation classes
         this.switcherTarget.classList.remove('translate-x-0');
         this.switcherTarget.classList.add('translate-x-5');
+
+        // button bg colour classes
+        this.themeToggleTarget.classList.remove('bg-gray-200');
+        this.themeToggleTarget.classList.add('bg-nepal-500');
       } else {
         localStorage.setItem('theme', 'light');
 
         document.documentElement.classList.remove('dark');
+
+        // animation classes
         this.switcherTarget.classList.remove('translate-x-5');
         this.switcherTarget.classList.add('translate-x-0');
+
+        // button bg colour classes
+        this.themeToggleTarget.classList.remove('bg-nepal-500');
+        this.themeToggleTarget.classList.add('bg-gray-200');
       }
     } else {
       // if NOT set via local storage previously
@@ -46,14 +73,26 @@ export default class extends Controller {
         localStorage.setItem('theme', 'light');
 
         document.documentElement.classList.remove('dark');
+
+        // animation classes
         this.switcherTarget.classList.remove('translate-x-5');
         this.switcherTarget.classList.add('translate-x-0');
+
+        // button bg colour classes
+        this.themeToggleTarget.classList.remove('bg-nepal-500');
+        this.themeToggleTarget.classList.add('bg-gray-200');
       } else {
         localStorage.setItem('theme', 'dark');
 
         document.documentElement.classList.add('dark');
+
+        // animation classes
         this.switcherTarget.classList.remove('translate-x-0');
         this.switcherTarget.classList.add('translate-x-5');
+
+        // button bg colour classes
+        this.themeToggleTarget.classList.remove('bg-gray-200');
+        this.themeToggleTarget.classList.add('bg-nepal-500');
       }
     }
   }
