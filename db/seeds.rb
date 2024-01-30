075f0b20ffb9d7a09dec20a7c77c074142ede016
Ellis Brookes <ellisbrookes98@gmail.com>
2024-01-30 02:49:31 +0000

    created customer table

--- db/seeds.rb
+++ db/seeds.rb
@@ -82,6 +82,20 @@ end
 
 puts 'Seeded 10 projects into the database'
 
+# Create customers
+10.times do |_n|
+  Customer.create(
+    full_name: Faker::Name.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    email: Faker::Internet.email,
+    notes: Faker::Lorem.sentence(word_count: 20),
+    company_id: Faker::Number.within(range: 1..9),
+  )
+end
+
+puts 'Seeded 10 customers into the database'
+
 # Create invoices
 10.times do |_n|
   Invoice.create(
