b096da48d7f8f21bdd235d98636a4de7f51c76d4
Ellis <ellisbrookes98@gmail.com>
2024-02-02 19:18:50 +0000

    fixed migrations

--- db/migrate/20240201003030_add_description_unit_price_to_items.rb
+++
@@ -1,6 +0,0 @@
-class AddDescriptionUnitPriceToItems < ActiveRecord::Migration[7.1]
-  def change
-    add_column :items, :description, :string
-    add_column :items, :unit_price, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_01_004657) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_30_201815) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -107,9 +107,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_01_004657) do
     t.bigint "company_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "descrption"
-    t.string "unit_price"
-    t.string "description"
     t.index ["company_id"], name: "index_items_on_company_id"
     t.index ["invoice_id"], name: "index_items_on_invoice_id"
   end
