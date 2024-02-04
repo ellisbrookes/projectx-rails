fae1797c1f98dc108d45d92b758dd199789e7798
Ellis <ellisbrookes98@gmail.com>
2024-02-04 21:44:20 +0000

    controller changes and lint fixes

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,4 +4,10 @@ class NotificationsController < ApplicationController
   def index
     @notifications = current_user.notifications.order(created_at: :desc)
   end
+
+  private
+
+  def notification_params
+    params.require(:notification).permit(:content)
+  end
 end
