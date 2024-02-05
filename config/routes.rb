014c972cc763005406b237c6329fe6f6c39d3885
Ellis <ellisbrookes98@gmail.com>
2024-02-05 02:38:18 +0000

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   resources :dashboard, only: [:index] do
     collection do
-      resources :notifications, only: [:index]
+      resources :notifications
       resources :companies do
         resources :customers
         resources :invoices, except: %i[destroy]
