798d8991b1b16e9b8ac4257a15c3ef635d7bb9f2
Ellis <ellisbrookes98@gmail.com>
2024-02-05 21:55:02 +0000

    lint fixes

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
 
   layout 'dashboard'
-  
+
   def index
     @notifications = current_user.notifications.order(created_at: :desc)
     add_breadcrumbs("Notifications", notifications_path)
