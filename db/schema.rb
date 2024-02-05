a37f7b6ef22e2a63f4e85e3ed2b328ecbd560cbc
Ellis Marcus Brookes <ellisbrookes98@gmail.com>
2024-02-05 19:25:34 +0000

    company changes (#36)
    * company logo creation
    
    * Added company logo not rendering correctly though
    
    * changes
    
    * added billing_email and contact_email to companies
    
    * model changes moved logo to the top of the page and changed company spec test

--- db/schema.rb
+++ db/schema.rb
@@ -52,7 +52,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_30_201815) do
   create_table "companies", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.string "email"
+    t.string "contact_email"
+    t.string "billing_email"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
