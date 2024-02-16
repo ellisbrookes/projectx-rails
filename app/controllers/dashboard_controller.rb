e06000782aa21dc08aef44ff77afdebdd9eeaa0f
Ellis <ellisbrookes98@gmail.com>
2024-02-16 19:34:01 +0000

    format fixes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,14 +4,14 @@ class DashboardController < ApplicationController
 
   def index
     @checkout_session_product1 = current_user.payment_processor.checkout(
-    mode: 'subscription',
-    line_items: [{
-      price: 'price_1OkDtdHhcsFflMX4DLJqziDA',
-      quantity: 1
-    }],
-    subscription_data: {
-      trial_period_days: 30,
-    },
-  )
+      mode: 'subscription',
+      line_items: [{
+        price: 'price_1OkDtdHhcsFflMX4DLJqziDA',
+        quantity: 1
+      }],
+      subscription_data: {
+        trial_period_days: 30
+      }
+    )
   end
 end
