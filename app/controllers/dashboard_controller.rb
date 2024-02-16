a16ee6bf7e91bb2fa553894d4bb1ea3476c2e3b1
Ellis <ellisbrookes98@gmail.com>
2024-02-16 19:30:48 +0000

    added stripe checkout to subscription and change a few things

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,15 @@ class DashboardController < ApplicationController
   layout 'dashboard'
 
   def index
+    @checkout_session_product1 = current_user.payment_processor.checkout(
+    mode: 'subscription',
+    line_items: [{
+      price: 'price_1OkDtdHhcsFflMX4DLJqziDA',
+      quantity: 1
+    }],
+    subscription_data: {
+      trial_period_days: 30,
+    },
+  )
   end
 end
