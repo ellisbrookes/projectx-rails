71a4c10cf3ce86023618df97a7ff138c37909dc2
Ellis <ellisbrookes98@gmail.com>
2024-02-20 19:28:14 +0000

    created two roles

--- config/initializers/rabarber.rb
+++ config/initializers/rabarber.rb
@@ -2,7 +2,16 @@ Rabarber.configure do |config|
   config.cache_enabled = true
   config.current_user_method = :current_user
   config.must_have_roles = false
-  config.when_actions_missing = -> (missing_actions, context) { ... }
-  config.when_roles_missing = -> (missing_roles, context) { ... }
-  config.when_unauthorized = -> (controller) { ... }
+
+  config.when_actions_missing = ->(missing_actions, context) do
+    raise ActionController::RoutingError.new('Not Found')
+  end
+
+  config.when_roles_missing = ->(missing_roles, context) do
+    redirect_to dashboard_index_path, alert: 'You do not have sufficient permissions'
+  end
+
+  config.when_unauthorized = ->(controller) do
+    redirect_to dashboard_index_path, alert: 'You are not authorized to access this page'
+  end
 end
