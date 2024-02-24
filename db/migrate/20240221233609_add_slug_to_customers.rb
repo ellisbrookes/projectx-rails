60de16923d7c1391930d9a94deb987d02134467d
Ben Bagley <hello@benbagley.dev>
2024-02-24 19:55:27 +0000

    merge

--- db/migrate/20240221233609_add_slug_to_customers.rb
+++
@@ -1,6 +0,0 @@
-class AddSlugToCustomers < ActiveRecord::Migration[7.1]
-  def change
-    add_column(:customers, :slug, :string)
-    add_index(:customers, :slug, unique: true)
-  end
-end
--- db/migrate/20240224161713_remove_customer_from_invoices.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCustomerFromInvoices < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :invoices, :customer, :string
-  end
-end
