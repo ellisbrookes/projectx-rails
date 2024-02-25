bc6d423219d3c92f127895667705e1da8fca75b1
Ben Bagley <hello@benbagley.dev>
2024-02-25 22:17:09 +0000

    redo

--- app/notifiers/comment_notifier.rb
+++ app/notifiers/comment_notifier.rb
@@ -1,32 +1,14 @@
-# To deliver this notification:
-#
-# NewComment.with(record: @post, message: "New post").deliver(User.all)
-
 class CommentNotifier < ApplicationNotifier
-  # Add your delivery methods
-  #
   deliver_by :email do |config|
-    config.mailer = "UserMailer"
-    config.method = "new_comment"
-    config.params = ->(recipient) { {user: recipient} }
+    config.mailer = 'CommentMailer'
+    config.method = :new_comment
+    config.if = ->{ recipient.email_notifications? }
     config.wait = 5.minutes
   end
-  
+
   notification_methods do
     def message
-      "This is #{recipient.full_name}'s foo: #{params[:foo]}"
+      "#{params[:commenter_name]} commented on <a href='#{company_team_project_task_path(params[:task_id])}'>#{params[:comment_task]}</a>".html_safe
     end
   end
-  #
-  # bulk_deliver_by :slack do |config|
-  #   config.url = -> { Rails.application.credentials.slack_webhook_url }
-  # end
-  #
-  # deliver_by :custom do |config|
-  #   config.class = "MyDeliveryMethod"
-  # end
-
-  # Add required params
-  #
-  # required_param :message
 end
