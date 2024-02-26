a0536bf13d7113ad1761a21a4ae2b2720058c140
Ben Bagley <hello@benbagley.dev>
2024-02-26 02:23:11 +0000

    added pricing partials, added pricing comparision table, fixed instance methods

--- app/controllers/stripe/pricing_controller.rb
+++ app/controllers/stripe/pricing_controller.rb
@@ -1,5 +1,7 @@
 class Stripe::PricingController < ApplicationController
   def index
     @prices = Stripe::Price.list(active: true, expand: ['data.product']).data.sort_by(&:unit_amount)
+    @monthly = @prices.select { |price| price.recurring.interval == 'month' }
+    @yearly = @prices.select { |price| price.recurring.interval == 'year' }
   end
 end
