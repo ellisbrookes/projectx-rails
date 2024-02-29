fae22ccd3e6ee920795874f112ab8dd4d52b762e
Ben Bagley <hello@benbagley.dev>
2024-02-29 23:29:03 +0000

    fixed pricing table toggle

--- app/controllers/stripe/pricing_controller.rb
+++ app/controllers/stripe/pricing_controller.rb
@@ -2,8 +2,8 @@ module Stripe
   class PricingController < ApplicationController
     def index
       @prices = Stripe::Price.list(active: true, expand: ['data.product']).data.sort_by(&:unit_amount)
-      @monthly = @prices.select { |price| price.recurring.interval == 'month' }
-      @yearly = @prices.select { |price| price.recurring.interval == 'year' }
+      @monthly = @prices.select { |price| price.recurring.interval == 'month' }.sort_by(&:unit_amount)
+      @yearly = @prices.select { |price| price.recurring.interval == 'year' }.sort_by(&:unit_amount)
     end
   end
 end
