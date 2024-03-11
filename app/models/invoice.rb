6374ba45d11ff28e19d8270852f86a303a3ce21e
Ellis <ellisbrookes98@gmail.com>
2024-03-11 01:04:47 +0000

    started moving over stuff

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,8 +2,9 @@
 class Invoice < ApplicationRecord
   belongs_to :company
   belongs_to :customer
+  has_many :invoice_items
 
-  has_and_belongs_to_many :items
+  accepts_nested_attributes_for :invoice_items
 
   validates :invoice_issue, presence: true
   validates :customer_id, presence: true
@@ -12,15 +13,6 @@ class Invoice < ApplicationRecord
   validates :due_date, presence: true
   validates :company_address, presence: true
   validates :amount, presence: true
-
-  accepts_nested_attributes_for :items, reject_if: :all_blank, allow_destroy: true
-
-  def items_attributes=(item_attributes)
-    item_attributes.values.each do |item_attribute|
-      item = Item.find_or_create_by(item_attribute)
-      items << item
-    end
-  end
 
   def calculate_total_amount
     self.total_amount = items.sum(&:total_price)
